a84e1c60acbacf7df5b535702dddd0dd2debce71
Simon Tett <simon.tett@ed.ac.uk>
2023-07-26 16:13:57 +0100

    Update run_agg.sh
    Added a bit of documentation

--- transpose_script/run_agg.sh
+++ transpose_script/run_agg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # run aggregation script on annual datafiles. Assumed to be running on datastore
+# run as run_agg.sh experiment 
+# for example run_agg.sh xaaaa
+# This will need editing to reflect where you want data to go
 
 export HDF5_USE_FILE_LOCKING=FALSE # so netcdf reads etc work on datastore.
 
 dirname=$1 ; shift
-rootdir=/exports/csce/datastore/geos/groups/merlin/top/stett2/
-aggdir=$rootdir/AggData
+rootdir=/exports/csce/datastore/geos/groups/merlin/top/stett2/ # where data is found
+aggdir=$rootdir/AggData # where you want data to go.
 script=$aggdir/transpose_script/splitAgg.py #path to agg script
 for dir in apx opx opy apy
 do
